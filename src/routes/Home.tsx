9e2f3b9c8b92d0b4845e0d6fe445923e7ce88575
leonardo-luz <leonardoluz.f02@gmail.com>
2025-05-03 22:08:12 -0300

    feat: add focus on enter

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Tile } from "../components/Tile";
 import { Modal } from "../components/Modal";
 import words from "../assets/words.json"
@@ -11,6 +11,7 @@ export const Home = () => {
   const [tries, setTries] = useState<string[]>([]);
   const [win, setWin] = useState<boolean>(false);
   const [streak, setStreak] = useState<number>(0);
+  const inputRef = useRef<HTMLDivElement>(null);
 
   const TypeHandler = (event: React.KeyboardEvent<HTMLDivElement>) => {
     const key = event.key
@@ -57,70 +58,77 @@ export const Home = () => {
     setTarget(words[index])
   }, [])
 
+  useEffect(() => {
+    inputRef?.current?.focus();
+  }, [target])
+
   return (
-    <div
-      onKeyDown={TypeHandler} tabIndex={0}
-      className="flex flex-col w-full h-full gap-4 align-top justify-top p-12"
-    >
-      <div className="flex flex-row gap-3 self-center">
-        {
-          target.split("").map((letter, index) => <Tile key={index} letter={letter} img={`${letter}.png`} />)
-        }
-      </div>
-      <div className="flex flex-col gap-3 self-center">
-        {
-          tries.map(triedWord => {
-            return <div className="flex flex-row gap-3 self-center">
-              {
-                triedWord.split("").map((letter, index) => {
-                  const corret = target.charAt(index) == letter
-                  const contains = target.includes(letter)
-                  const wrong = !corret && !contains
-
-                  return <Tile
-                    corret={corret}
-                    contains={contains}
-                    wrong={wrong}
-                    key={index} letter={letter} />
-                })
-              }
-            </div>
-          })
-        }
-      </div>
-      <div className="flex flex-row gap-3 self-center">
-        {
-          word.split("").map((letter, index) => <Tile key={index} letter={letter} />)
-        }
-      </div>
-
-      {
-        win &&
-        <Modal>
-          <h1 className="self-center text-3xl text-white font-extrabold">VITORIA!!!</h1>
-          <h1 className="self-center text-sm text-white font-extrabold">Você acertou em {tries.length} Tentativas</h1>
+    target ?
+      <div
+        ref={inputRef}
+        onKeyDown={TypeHandler} tabIndex={0}
+        className="flex outline-0 flex-col w-full h-full gap-4 align-top justify-top p-12"
+      >
+        <div className="flex flex-row gap-3 self-center">
           {
-            streak > 0 &&
-            <h1 className="self-center text-sm text-white font-extrabold">Sequência de acertos: {streak}</h1>
+            target.split("").map((letter, index) => <Tile key={index} letter={letter} img={`${letter}.png`} />)
           }
-          <hr className="border-white w-full self-center border-2" />
-          <div className="w-full flex flex-row justify-around align-middle">
-            <input onClick={ExitHandler} className="bg-white font-bold text-black p-3 rounded-md cursor-pointer shadow-black shadow-md hover:shadow-transparent" type="button" value="SAIR" />
-            <input onClick={ContinueHandler} className="bg-white font-bold text-black p-3 rounded-md cursor-pointer shadow-black shadow-md hover:shadow-transparent" type="button" value="CONTINUAR" />
-          </div>
-        </Modal>
-      }
-      {
-        (!win && tries.length == QTY_TRIES) &&
-        <Modal>
-          <h1 className="self-center text-3xl text-white font-extrabold">DERROTA...</h1>
-          <h1 className="self-center text-sm text-white font-extrabold">A palavra era {target} Tentativas</h1>
-          <hr className="border-white w-full self-center border-2" />
-          <div className="w-full flex flex-row justify-around align-middle">
-            <input onClick={ExitHandler} className="bg-white font-bold text-black p-3 rounded-md cursor-pointer shadow-black shadow-md hover:shadow-transparent" type="button" value="TENTAR NOVAMENTE" />
-          </div>
-        </Modal>
-      }
-    </div>
+        </div>
+        <div className="flex flex-col gap-3 self-center">
+          {
+            tries.map(triedWord => {
+              return <div className="flex flex-row gap-3 self-center">
+                {
+                  triedWord.split("").map((letter, index) => {
+                    const corret = target.charAt(index) == letter
+                    const contains = target.includes(letter)
+                    const wrong = !corret && !contains
+
+                    return <Tile
+                      corret={corret}
+                      contains={contains}
+                      wrong={wrong}
+                      key={index} letter={letter} />
+                  })
+                }
+              </div>
+            })
+          }
+        </div>
+        <div className="flex flex-row gap-3 self-center">
+          {
+            word.split("").map((letter, index) => <Tile key={index} letter={letter} />)
+          }
+        </div>
+
+        {
+          win &&
+          <Modal>
+            <h1 className="self-center text-3xl text-white font-extrabold">VITORIA!!!</h1>
+            <h1 className="self-center text-sm text-white font-extrabold">Você acertou em {tries.length} Tentativas</h1>
+            {
+              streak > 0 &&
+              <h1 className="self-center text-sm text-white font-extrabold">Sequência de acertos: {streak}</h1>
+            }
+            <hr className="border-white w-full self-center border-2" />
+            <div className="w-full flex flex-row justify-around align-middle">
+              <input onClick={ExitHandler} className="bg-white font-bold text-black p-3 rounded-md cursor-pointer shadow-black shadow-md hover:shadow-transparent" type="button" value="SAIR" />
+              <input onClick={ContinueHandler} className="bg-white font-bold text-black p-3 rounded-md cursor-pointer shadow-black shadow-md hover:shadow-transparent" type="button" value="CONTINUAR" />
+            </div>
+          </Modal>
+        }
+        {
+          (!win && tries.length == QTY_TRIES) &&
+          <Modal>
+            <h1 className="self-center text-3xl text-white font-extrabold">DERROTA...</h1>
+            <h1 className="self-center text-sm text-white font-extrabold">A palavra era {target} Tentativas</h1>
+            <hr className="border-white w-full self-center border-2" />
+            <div className="w-full flex flex-row justify-around align-middle">
+              <input onClick={ExitHandler} className="bg-white font-bold text-black p-3 rounded-md cursor-pointer shadow-black shadow-md hover:shadow-transparent" type="button" value="TENTAR NOVAMENTE" />
+            </div>
+          </Modal>
+        }
+      </div> :
+      <div>loading</div>
   )
 }
