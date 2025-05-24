e87bd8cb1566b6104b3f0572a31a5516ff925f57
Leonardo Luz Fachel <leonardoluz.f02@gmail.com>
2025-05-24 09:13:37 -0300

    feat: add highscore and word position placeholder

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -11,6 +11,7 @@ export const Home = () => {
   const [tries, setTries] = useState<string[]>([]);
   const [win, setWin] = useState<boolean>(false);
   const [streak, setStreak] = useState<number>(0);
+  const [highscore, setHighscore] = useState<number>(0);
   const inputRef = useRef<HTMLDivElement>(null);
 
   const TypeHandler = (event: React.KeyboardEvent<HTMLDivElement>) => {
@@ -21,7 +22,7 @@ export const Home = () => {
     else if (key == "Enter" && word.length == target.length && tries.length < QTY_TRIES) {
       setWord("")
       if (word == target) {
-        setWin(true)
+        return ContinueHandler()
       }
       setTries(prev => {
         return [...prev, word]
@@ -41,7 +42,7 @@ export const Home = () => {
     setWin(false);
   }
 
-  function ContinueHandler(_: React.MouseEvent<HTMLInputElement, MouseEvent>): void {
+  function ContinueHandler(): void {
     reset()
     const index = Math.floor(Math.random() * words.length)
     setTarget(words[index])
@@ -62,6 +63,11 @@ export const Home = () => {
     inputRef?.current?.focus();
   }, [target])
 
+  useEffect(() => {
+    if (streak > highscore)
+      setHighscore(streak)
+  }, [streak])
+
   return (
     target ?
       <div
@@ -69,6 +75,10 @@ export const Home = () => {
         onKeyDown={TypeHandler} tabIndex={0}
         className="flex outline-0 flex-col w-full h-full gap-4 align-top justify-top p-12"
       >
+        <div className="self-center flex flex-row justify-between p-4 w-[70%]">
+          <p>Highscore: {highscore}</p>
+          <p>Streak: {streak}</p>
+        </div>
         <div className="flex flex-row gap-3 self-center">
           {
             target.split("").map((letter, index) => <Tile key={index} letter={letter} img={`${letter}.png`} />)
@@ -99,29 +109,15 @@ export const Home = () => {
           {
             word.split("").map((letter, index) => <Tile key={index} letter={letter} />)
           }
+          {
+            target.substring(word.length).split("").map((_, index) => <Tile key={index} letter={""} />)
+          }
         </div>
-
-        {
-          win &&
-          <Modal>
-            <h1 className="self-center text-3xl text-white font-extrabold">VITORIA!!!</h1>
-            <h1 className="self-center text-sm text-white font-extrabold">Você acertou em {tries.length} Tentativas</h1>
-            {
-              streak > 0 &&
-              <h1 className="self-center text-sm text-white font-extrabold">Sequência de acertos: {streak}</h1>
-            }
-            <hr className="border-white w-full self-center border-2" />
-            <div className="w-full flex flex-row justify-around align-middle">
-              <input onClick={ExitHandler} className="bg-white font-bold text-black p-3 rounded-md cursor-pointer shadow-black shadow-md hover:shadow-transparent" type="button" value="SAIR" />
-              <input onClick={ContinueHandler} className="bg-white font-bold text-black p-3 rounded-md cursor-pointer shadow-black shadow-md hover:shadow-transparent" type="button" value="CONTINUAR" />
-            </div>
-          </Modal>
-        }
         {
           (!win && tries.length == QTY_TRIES) &&
           <Modal>
-            <h1 className="self-center text-3xl text-white font-extrabold">DERROTA...</h1>
-            <h1 className="self-center text-sm text-white font-extrabold">A palavra era {target} Tentativas</h1>
+            <h1 className="self-center text-4xl text-white font-extrabold">DERROTA...</h1>
+            <h1 className="self-center text-2xl text-white font-extrabold">A palavra era {target}</h1>
             <hr className="border-white w-full self-center border-2" />
             <div className="w-full flex flex-row justify-around align-middle">
               <input onClick={ExitHandler} className="bg-white font-bold text-black p-3 rounded-md cursor-pointer shadow-black shadow-md hover:shadow-transparent" type="button" value="TENTAR NOVAMENTE" />
