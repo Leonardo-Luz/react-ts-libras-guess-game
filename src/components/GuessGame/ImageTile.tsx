2655d748d53e5f7879bd3049757098b295c31ef8
gustavoprs <gustavopolicarporsch@gmail.com>
2025-06-11 11:36:04 -0300

    feat: update letter sign images and adjust styles for new proportions

--- src/components/GuessGame/ImageTile.tsx
+++ src/components/GuessGame/ImageTile.tsx
@@ -9,7 +9,7 @@ export default function ImageTile({ letter, className }: ImageTileProps){
 
 	return (
 		<div className={cn(
-			"rounded-md flex-1 max-w-28 aspect-[7/8] bg-muted overflow-hidden", className
+			"rounded-md flex-1 max-w-28 aspect-square bg-muted overflow-hidden", className
 		)}>
 			<img  src={`/${letter.toUpperCase()}.png`} alt={`Imagem da letra ${letter} em libras`} className="w-full h-full object-fill" />
 		</div>
