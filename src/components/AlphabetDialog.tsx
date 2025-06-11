09c57f2f357efbaff2f42034e7d8982dc28cf317
gustavoprs <gustavopolicarporsch@gmail.com>
2025-06-11 11:19:03 -0300

    style: change the responsiveness of AlphabetDialog

--- src/components/AlphabetDialog.tsx
+++ src/components/AlphabetDialog.tsx
@@ -18,133 +18,115 @@ export default function AlphabetDialog({ children }: DictionaryDialogProps) {
 					<DialogTitle className=" text-xl">Alfabeto</DialogTitle>
 				</DialogHeader>
 				<p className="mb-2">Quando um símbolo inclui movimento, ele é indicado por setas que mostram a direção do gesto.</p>
-				<div className=" flex flex-col gap-4 items-center justify-start max-h-[40vh] overflow-y-scroll">
-					<div className="flex flex-row gap-8">
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"A"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">A</span>
+				<div className="grid grid-cols-2 gap-4 max-h-[40vh] overflow-y-scroll sm:grid-cols-3">
+						<label className="flex gap-2">
+							<ImageTile letter={"A"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">A</span>
 						</label>
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"B"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">B</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"B"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">B</span>
 						</label>
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"C"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">C</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"C"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">C</span>
 						</label>
-					</div>
-					<div className="flex flex-row gap-8">
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"D"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">D</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"D"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">D</span>
 						</label>
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"E"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">E</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"E"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">E</span>
 						</label>
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"F"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">F</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"F"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">F</span>
 						</label>
-					</div>
-					<div className="flex flex-row gap-8">
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"G"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">G</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"G"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">G</span>
 						</label>
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"H"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">H</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"H"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">H</span>
 						</label>
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"I"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">I</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"I"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">I</span>
 						</label>
-					</div>
-					<div className="flex flex-row gap-8">
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"J"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">J</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"J"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">J</span>
 						</label>
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"K"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">K</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"K"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">K</span>
 						</label>
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"L"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">L</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"L"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">L</span>
 						</label>
-					</div>
-					<div className="flex flex-row gap-8">
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"M"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">M</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"M"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">M</span>
 						</label>
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"N"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">N</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"N"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">N</span>
 						</label>
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"O"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">O</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"O"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">O</span>
 						</label>
-					</div>
-					<div className="flex flex-row gap-8">
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"P"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">P</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"P"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">P</span>
 						</label>
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"Q"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">Q</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"Q"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">Q</span>
 						</label>
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"R"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">R</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"R"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">R</span>
 						</label>
-					</div>
-					<div className="flex flex-row gap-8">
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"S"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">S</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"S"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">S</span>
 						</label>
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"T"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">T</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"T"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">T</span>
 						</label>
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"U"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">U</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"U"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">U</span>
 						</label>
-					</div>
-					<div className="flex flex-row gap-8">
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"V"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">V</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"V"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">V</span>
 						</label>
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"W"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">W</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"W"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">W</span>
 						</label>
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"X"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">X</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"X"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">X</span>
 						</label>
-					</div>
-					<div className="flex flex-row gap-8">
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"Y"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">Y</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"Y"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">Y</span>
 						</label>
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"Z"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">Z</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"Z"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">Z</span>
 						</label>
-						<label className="flex flex-row gap-3">
-							<ImageTile letter={"Ç"} className=" max-w-14 min-w-14" />
-							<span className="inline-flex justify-center items-center rounded-md w-14 h-16 text-3xl font-bold bg-correct text-correct-foreground">Ç</span>
+						<label className="flex gap-2">
+							<ImageTile letter={"Ç"} className=" flex-1 max-w-[initial] aspect-[7/8]" />
+							<span className="inline-flex justify-center items-center rounded-md flex-1 text-3xl font-bold bg-correct text-correct-foreground">Ç</span>
 						</label>
-					</div>
 				</div>
 			</DialogContent>
 		</Dialog>
