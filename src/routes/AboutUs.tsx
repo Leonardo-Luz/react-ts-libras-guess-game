96a2fa01cadd32819d841cb062be2a5f7d997d88
gustavoprs <gustavopolicarporsch@gmail.com>
2025-06-13 10:21:28 -0300

    style: change <hr> colors to default on about-us page

--- src/routes/AboutUs.tsx
+++ src/routes/AboutUs.tsx
@@ -21,7 +21,7 @@ export const AboutUs = () => {
 				<div className="flex flex-col gap-4 my-12 rounded-lg p-4 bg-accent">
 					<div className="flex flex-col gap-2">
 						<h1 className="self-center text-4xl text-primary font-extrabold">SOBRE NÓS</h1>
-						<hr className="w-full border-primary border-1" />
+						<hr className="w-full border-1" />
 					</div>
 					<p className="w-full self-center text-justify text-1xl">
 						Website desenvolvido por <strong className="text-primary">Leonardo Luz</strong>, <strong className="text-primary">Diego Prestes</strong>, <strong className="text-primary">Gustavo Policarpo</strong> & <strong className="text-primary">Gustavo Martins</strong> durante o curso de Análise e Desenvolvimento de Sistemas no <em>Instituo Federal do Rio Grande do Sul - campus Osório</em>
@@ -29,7 +29,7 @@ export const AboutUs = () => {
 					<p className="w-full self-center text-justify text-1xl">
 						Este projeto é um jogo interativo desenvolvido como parte da disciplina de Libras, com o objetivo de auxiliar no aprendizado do alfabeto manual da Língua Brasileira de Sinais (Libras).
 					</p>
-					<hr className="w-full border-primary border-1" />
+					<hr className="w-full border-1" />
 					<p className="w-full self-center text-justify text-1xl">
 						Inspirado no popular jogo <a
 							href="https://term.ooo"
